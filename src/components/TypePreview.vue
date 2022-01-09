87ddfa6a5c01126623722f9ca2a336703cd782bc
Brian <bdnorris@gmail.com>
2022-01-09 14:18:26 -0600

    add sass and a heading

--- src/components/TypePreview.vue
+++ src/components/TypePreview.vue
@@ -20,6 +20,7 @@
     <pre>font-size: clamp({{minSizeRem}}rem, calc(1rem + {{fluidSize}}vw), {{maxSizeRem}}rem);</pre>
   </div>
 	<div class="preview" ref="preview">
+    <h1>Heading Test</h1>
     <p>Pitch upon the very middle of the plat where the town or line of houses is to be laid or run, facing the harbor of the great river, for the situation of my house; ... the distance of each house from the creek or harbor should be, in my judgment, a measured quarter of a mile; or, at least, two hundred paces, because of building hereafter streets down to the harbor.” Such were the instructions which William Penn, founder of Philadelphia, gave to his commissioners, William Crispin, John Bezar, and Nathaniel Allen, for the building of what is now known as Penn’s Cottage.</p>
     <p>It was in 1681 that the great Quaker completed the negotiations for the grant of Pennsylvania, and in the next year the first work of the building of the Proprietary House was begun. The plat chosen for its site was the one bounded by Front, Chestnut, Letitia, and High streets, the last now being named Market. In the place of the little cottage and its surrounding yard there is, to-day, one of the most thickly-built portions of Philadelphia. But the true centre of the city, at one time radiating from this point, has now, owing to the growth of two hundred years, moved a mile to the westward.</p>
   </div>
@@ -57,7 +58,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 [draggable=true] {
   cursor: move;
 }
@@ -69,15 +70,15 @@ export default {
 	/* max-width: 85%; */
 	/* overflow: scroll; */
 	/* resize: both; */
-}
-p {
-  text-align: left;
-  line-height: 1.333;
-  font-size: clamp(
-    var(--min-size, 1rem),
-    calc(1rem + var(--fluid-size, 3vw)),
-    var(--max-size, 2rem)
-  );
+  h1, p {
+    text-align: left;
+    line-height: 1.333;
+    font-size: clamp(
+      var(--min-size, 1rem),
+      calc(1rem + var(--fluid-size, 3vw)),
+      var(--max-size, 2rem)
+    );
+  }
 }
 pre {
   /* text-align: left; */

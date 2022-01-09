46810c073ca9ca29e72a0c176a014d4beefbed63
Brian <bdnorris@gmail.com>
2022-01-09 14:09:16 -0600

    show the code

--- src/components/TypePreview.vue
+++ src/components/TypePreview.vue
@@ -16,10 +16,12 @@
       {{ maxSize }}px
     </div>
   </div>
+  <div class="code">
+    <pre>font-size: clamp({{minSizeRem}}rem, calc(1rem + {{fluidSize}}vw), {{maxSizeRem}}rem);</pre>
+  </div>
 	<div class="preview" ref="preview">
     <p>Pitch upon the very middle of the plat where the town or line of houses is to be laid or run, facing the harbor of the great river, for the situation of my house; ... the distance of each house from the creek or harbor should be, in my judgment, a measured quarter of a mile; or, at least, two hundred paces, because of building hereafter streets down to the harbor.” Such were the instructions which William Penn, founder of Philadelphia, gave to his commissioners, William Crispin, John Bezar, and Nathaniel Allen, for the building of what is now known as Penn’s Cottage.</p>
     <p>It was in 1681 that the great Quaker completed the negotiations for the grant of Pennsylvania, and in the next year the first work of the building of the Proprietary House was begun. The plat chosen for its site was the one bounded by Front, Chestnut, Letitia, and High streets, the last now being named Market. In the place of the little cottage and its surrounding yard there is, to-day, one of the most thickly-built portions of Philadelphia. But the true centre of the city, at one time radiating from this point, has now, owing to the growth of two hundred years, moved a mile to the westward.</p>
-    <p>According to one tradition, the Penn or Letitia House was the first brick building erected in Philadelphia; to another, it was the first house to have a cellar. The name, “Letitia,” was given to it by Penn himself, as the house was intended eventually to be the portion of his daughter, Letitia. It is from this source, too, that Letitia Street gets its name.</p>
   </div>
 </template>
 
@@ -34,10 +36,10 @@ export default {
     }
   },
   computed: {
-    minSizePx() {
+    minSizeRem() {
       return this.minSize / 16
     },
-    maxSizePx() {
+    maxSizeRem() {
       return this.maxSize / 16
     }
   },
@@ -45,10 +47,10 @@ export default {
     fluidSize(newValue) {
       this.$refs.preview.style.setProperty('--fluid-size', newValue  + 'vw')
     },
-    minSizePx(newValue) {
+    minSizeRem(newValue) {
       this.$refs.preview.style.setProperty('--min-size', newValue + 'rem')
     },
-    maxSizePx(newValue) {
+    maxSizeRem(newValue) {
       this.$refs.preview.style.setProperty('--max-size', newValue + 'rem')
     },
   },
@@ -63,17 +65,25 @@ export default {
   --fluid-size: 3vw;
   --min-size: 1rem;
   --max-size: 2.5rem;
-	padding: 1em max(1.5rem, (50vw - 960px) / 2);
+	padding: 1em max(2rem, (50vw - 960px) / 2);
 	/* max-width: 85%; */
 	/* overflow: scroll; */
 	/* resize: both; */
 }
 p {
-    font-size: clamp(
+  text-align: left;
+  line-height: 1.333;
+  font-size: clamp(
     var(--min-size, 1rem),
     calc(1rem + var(--fluid-size, 3vw)),
     var(--max-size, 2rem)
   );
+}
+pre {
+  /* text-align: left; */
+  background-color: rgb(25, 26, 27);
+  color: white;
+  padding: 1em;
 }
 
 </style>

255ffc75173788c53d96479f15d38afc01d3d766
Brian <bdnorris@gmail.com>
2022-01-09 22:35:29 -0600

    controls component

---
+++ src/components/Controls.vue
@@ -0,0 +1,93 @@
+<template>
+	<div class="">
+		<div class="controls__item">
+			<label for="font-stack">Font</label>
+			<input type="text" id="font-stack" v-model="_font" />
+		</div>
+		<div>
+			<label for="fluid-size">Fluid Size</label>
+			<input
+				type="range"
+				id="fluid-size"
+				min="1"
+				max="20"
+				step="0.1"
+				v-model="_fluidSize"
+			/>
+			{{ fluidSize }}vw
+		</div>
+		<div>
+			<label for="min-size">Min Size</label>
+			<input
+				type="range"
+				id="min-size"
+				min="1"
+				max="100"
+				step="1"
+				v-model="_minSize"
+			/>
+			{{ minSize }}px
+		</div>
+		<div>
+			<label for="max-size">Max Size</label>
+			<input
+				type="range"
+				id="max-size"
+				min="1"
+				max="100"
+				step="1"
+				v-model="_maxSize"
+			/>
+			{{ maxSize }}px
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Controls',
+	data() {
+    return {
+      _fluidSize: this.fluidSize,
+      _minSize: this.minSize,
+      _maxSize: this.maxSize,
+      _font: this.font,
+    }
+  },
+	props: {
+		fluidSize: {
+			type: [Number, String],
+			default: 2
+		},
+		minSize: {
+			type: [Number, String],
+			default: 16
+		},
+		maxSize: {
+			type: [Number, String],
+			default: 40
+		},
+		font: {
+			type: String,
+			default: 'sans-serif'
+		}
+	},
+	watch: {
+		_fluidSize(newValue) {
+			this.$emit('fluidSizeChange', newValue)
+		},
+		_minSize(newValue) {
+			this.$emit('minSizeChange', newValue)
+		},
+		_maxSize(newValue) {
+			this.$emit('maxSizeChange', newValue)
+		},
+		_font(newValue) {
+			this.$emit('fontChange', newValue)
+		}
+	},
+}
+</script>
+
+<style>
+</style>
--- src/components/TypePreview.vue
+++ src/components/TypePreview.vue
@@ -1,107 +1,241 @@
 <template>
   <div class="controls">
-    <div class="controls__item">
-      <label for="font-stack">Font</label>
-      <input type="text" id="font-stack" v-model="fontStack">
+    <div>
+      <h1>Headline</h1>
+      <Controls
+        @fontChange="updateHeadlineFont"
+        :font="headlineFont"
+        @fluidSizeChange="updateHeadlineFluid"
+        :fluid-size=fluidHeadlineSize
+        @minSizeChange="updateHeadlineMinSize"
+        :min-size=minHeadlineSize
+        @maxSizeChange="updateHeadlineMaxSize"
+        :max-size=maxHeadlineSize
+      />
     </div>
     <div>
-      <label for="fluid-size">Fluid Size</label>
-      <input type="range" id="fluid-size" min="1" max="20" step="0.1" v-model="fluidSize" />
-      {{ fluidSize }}vw
-    </div>
-    <div>
-      <label for="min-size">Min Size</label>
-      <input type="range" id="min-size" min="1" max="100" step="1" v-model="minSize" />
-      {{ minSize }}px
-    </div>
-    <div>
-      <label for="max-size">Max Size</label>
-      <input type="range" id="max-size" min="1" max="100" step="1" v-model="maxSize" />
-      {{ maxSize }}px
+      <h1>Paragraphs</h1>
+      <Controls
+        @fontChange="updateParagraphFont"
+        :font="paragraphFont"
+        @fluidSizeChange="updateParagraphFluid"
+        :fluid-size=fluidParagraphSize
+        @minSizeChange="updateParagraphMinSize"
+        :min-size=minParagraphSize
+        @maxSizeChange="updateParagraphMaxSize"
+        :max-size=maxParagraphSize
+      />
     </div>
   </div>
-  <div class="code">
-    <pre>font-size: clamp({{minSizeRem}}rem, calc(1rem + {{fluidSize}}vw), {{maxSizeRem}}rem);</pre>
+  <div class="code-wrap">
+    <div class="code">
+      <h2>Headline</h2>
+      <pre>font-size: clamp({{ minHeadlineSizeRem }}rem, calc(1rem + {{ fluidHeadlineSize }}vw), {{ maxHeadlineSizeRem }}rem);</pre>
+    </div>
+    <div class="code">
+      <h2>Paragraph</h2>
+      <pre>font-size: clamp({{ minParagraphSizeRem }}rem, calc(1rem + {{ fluidParagraphSize }}vw), {{ maxParagraphSizeRem }}rem);</pre>
+    </div>
   </div>
-	<div class="preview" ref="preview">
+  <div class="preview" ref="preview">
     <h1>Heading Test</h1>
-    <p>Pitch upon the very middle of the plat where the town or line of houses is to be laid or run, facing the harbor of the great river, for the situation of my house; ... the distance of each house from the creek or harbor should be, in my judgment, a measured quarter of a mile; or, at least, two hundred paces, because of building hereafter streets down to the harbor.” Such were the instructions which William Penn, founder of Philadelphia, gave to his commissioners, William Crispin, John Bezar, and Nathaniel Allen, for the building of what is now known as Penn’s Cottage.</p>
-    <p>It was in 1681 that the great Quaker completed the negotiations for the grant of Pennsylvania, and in the next year the first work of the building of the Proprietary House was begun. The plat chosen for its site was the one bounded by Front, Chestnut, Letitia, and High streets, the last now being named Market. In the place of the little cottage and its surrounding yard there is, to-day, one of the most thickly-built portions of Philadelphia. But the true centre of the city, at one time radiating from this point, has now, owing to the growth of two hundred years, moved a mile to the westward.</p>
+    <p>
+      Pitch upon the very middle of the plat where the town or line of houses is
+      to be laid or run, facing the harbor of the great river, for the situation
+      of my house; ... the distance of each house from the creek or harbor
+      should be, in my judgment, a measured quarter of a mile; or, at least, two
+      hundred paces, because of building hereafter streets down to the harbor.”
+      Such were the instructions which William Penn, founder of Philadelphia,
+      gave to his commissioners, William Crispin, John Bezar, and Nathaniel
+      Allen, for the building of what is now known as Penn’s Cottage.
+    </p>
+    <p>
+      It was in 1681 that the great Quaker completed the negotiations for the
+      grant of Pennsylvania, and in the next year the first work of the building
+      of the Proprietary House was begun. The plat chosen for its site was the
+      one bounded by Front, Chestnut, Letitia, and High streets, the last now
+      being named Market. In the place of the little cottage and its surrounding
+      yard there is, to-day, one of the most thickly-built portions of
+      Philadelphia. But the true centre of the city, at one time radiating from
+      this point, has now, owing to the growth of two hundred years, moved a
+      mile to the westward.
+    </p>
   </div>
 </template>
 
 <script>
+import Controls from "./Controls.vue";
+
 export default {
-  name: 'TypePreview',
-  data() {
-    return {
-      fluidSize: 2,
-      minSize: 16,
-      maxSize: 40,
-      fontStack: 'sans-serif',
-    }
-  },
-  computed: {
-    minSizeRem() {
-      return this.minSize / 16
-    },
-    maxSizeRem() {
-      return this.maxSize / 16
-    },
-    fontStackProcessed() {
-      if (this.fontStack.indexOf(' ') > -1) {
-        return '\'' + this.fontStack + '\''
-      } else {
-        return this.fontStack
-      }
-    },
-  },
-  watch: {
-    fluidSize(newValue) {
-      this.$refs.preview.style.setProperty('--fluid-size', newValue  + 'vw')
-    },
-    minSizeRem(newValue) {
-      this.$refs.preview.style.setProperty('--min-size', newValue + 'rem')
-    },
-    maxSizeRem(newValue) {
-      this.$refs.preview.style.setProperty('--max-size', newValue + 'rem')
-    },
-    fontStackProcessed(newValue) {
-      this.$refs.preview.style.setProperty('--font-family', newValue)
-    }
-  },
-}
+	name: "TypePreview",
+	components: {
+		Controls,
+	},
+	data() {
+		return {
+			fluidHeadlineSize: 4,
+			minHeadlineSize: 22,
+			maxHeadlineSize: 72,
+			fluidParagraphSize: 2,
+			minParagraphSize: 16,
+			maxParagraphSize: 40,
+			headlineFont: "sans-serif",
+			paragraphFont: "serif",
+		};
+	},
+	computed: {
+		minHeadlineSizeRem() {
+			return this.minHeadlineSize / 16;
+		},
+		maxHeadlineSizeRem() {
+			return this.maxHeadlineSize / 16;
+		},
+		minParagraphSizeRem() {
+			return this.minParagraphSize / 16;
+		},
+		maxParagraphSizeRem() {
+			return this.maxParagraphSize / 16;
+		},
+		headlineFontStackProcessed() {
+			if (this.headlineFont.indexOf(" ") > -1) {
+				return "'" + this.headlineFont + "'";
+			} else {
+				return this.headlineFont;
+			}
+		},
+		paragraphFontStackProcessed() {
+			if (this.paragraphFont.indexOf(" ") > -1) {
+				return "'" + this.paragraphFont + "'";
+			} else {
+				return this.paragraphFont;
+			}
+		},
+	},
+	watch: {
+		fluidHeadlineSize(newValue) {
+			this.$refs.preview.style.setProperty(
+				"--fluid-headline-size",
+				newValue + "vw"
+			);
+		},
+		fluidParagraphSize(newValue) {
+			this.$refs.preview.style.setProperty(
+				"--fluid-paragraph-size",
+				newValue + "vw"
+			);
+		},
+		minHeadlineSizeRem(newValue) {
+			this.$refs.preview.style.setProperty(
+				"--min-headline-size",
+				newValue + "rem"
+			);
+		},
+		minParagraphSizeRem(newValue) {
+			this.$refs.preview.style.setProperty(
+				"--min-paragraph-size",
+				newValue + "rem"
+			);
+		},
+		maxHeadlineSizeRem(newValue) {
+			this.$refs.preview.style.setProperty(
+				"--max-headline-size",
+				newValue + "rem"
+			);
+		},
+		maxParagraphSizeRem(newValue) {
+			this.$refs.preview.style.setProperty(
+				"--max-paragraph-size",
+				newValue + "rem"
+			);
+		},
+		headlineFontStackProcessed(newValue) {
+			this.$refs.preview.style.setProperty("--font-headline-family", newValue);
+		},
+		paragraphFontStackProcessed(newValue) {
+			this.$refs.preview.style.setProperty("--font-paragraph-family", newValue);
+		},
+	},
+	methods: {
+		updateHeadlineFont(value) {
+			this.headlineFont = value;
+		},
+		updateParagraphFont(value) {
+			this.paragraphFont = value;
+		},
+		updateHeadlineFluid(value) {
+			this.fluidHeadlineSize = value;
+		},
+		updateParagraphFluid(value) {
+			this.fluidParagraphSize = value;
+		},
+		updateHeadlineMinSize(value) {
+			this.minHeadlineSize = value;
+		},
+		updateParagraphMinSize(value) {
+			this.minParagraphSize = value;
+		},
+		updateHeadlineMaxSize(value) {
+			this.maxHeadlineSize = value;
+		},
+		updateParagraphMaxSize(value) {
+			this.maxParagraphSize = value;
+		},
+	},
+};
 </script>
 
 <style lang="scss">
-[draggable=true] {
-  cursor: move;
+[draggable="true"] {
+	cursor: move;
 }
 .preview {
-  --fluid-size: 3vw;
-  --min-size: 1rem;
-  --max-size: 2.5rem;
-  --font-family: sans-serif;
+	--fluid-headline-size: 4vw;
+	--fluid-paragraph-size: 2vw;
+	--min-headline-size: 1.375rem;
+	--min-paragraph-size: 1rem;
+	--max-headline-size: 4.5rem;
+	--max-paragraph-size: 2.5rem;
+	--font-headline-family: sans-serif;
+	--font-paragraph-family: serif;
 	padding: 1em max(2rem, (50vw - 960px) / 2);
 	/* max-width: 85%; */
 	/* overflow: scroll; */
 	/* resize: both; */
-  h1, p {
-    text-align: left;
-    line-height: 1.333;
-    font-family: var(--font-family);
-    font-size: clamp(
-      var(--min-size, 1rem),
-      calc(1rem + var(--fluid-size, 3vw)),
-      var(--max-size, 2rem)
-    );
-  }
+	h1,
+	p {
+		text-align: left;
+		line-height: 1.333;
+		transition: font-size 200ms ease-in-out;
+	}
+	h1 {
+		font-family: var(--font-headline-family);
+		font-size: clamp(
+			var(--min-headline-size, 1rem),
+			calc(1rem + var(--fluid-headline-size, 3vw)),
+			var(--max-headline-size, 2rem)
+		);
+	}
+	p {
+		font-family: var(--font-paragraph-family);
+		font-size: clamp(
+			var(--min-paragraph-size, 1rem),
+			calc(1rem + var(--fluid-paragraph-size, 3vw)),
+			var(--max-paragraph-size, 2rem)
+		);
+	}
 }
 pre {
-  /* text-align: left; */
-  background-color: rgb(25, 26, 27);
-  color: white;
-  padding: 1em;
+	/* text-align: left; */
+	background-color: rgb(25, 26, 27);
+	color: white;
+	padding: 1em;
+  text-align: center;
 }
-
+.controls,
+.code-wrap {
+	display: flex;
+	justify-content: space-around;
+	padding: 1em max(2rem, (50vw - 960px) / 2);
+  gap: 1em;
+}
 </style>
